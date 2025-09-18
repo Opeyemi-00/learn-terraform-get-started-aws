e444058dd1ea3e4297ef768f24e43c11ce040988
Opeyemi-00 <wethankful02@gmail.com>
2025-09-18 19:34:26 +0100

    initial again

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ module "vpc" {
 	cidr = "10.0.0.0/16"
 
     azs = ["eu-west-a", "eu-west-b", eu-west-c]
-    private_subnet = ["10.0.1.0/24", "10.0.2.0/24"]
-    public_subnet = ["10.0.101.0/24"]
+    private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+    public_subnets = ["10.0.101.0/24"]
 
 enable_dns_hostnames = true
 
